c9c7b1ee896e9e577f505f2f4a6edbb82ac17b47
Josh Ellen <joshshua989@gmail.com>
2025-07-25 13:19:43 -0400

    Update stat_loader.py

--- stat_loader.py
+++ stat_loader.py
@@ -2,4 +2,13 @@
 import pandas as pd
 
 def load_csv(filepath):
-    return pd.read_csv(filepath)
+    import os
+    df = pd.read_csv(filepath)
+    sample = df.iloc[0]
+
+    print(f"\n(SAMPLE) {os.path.normpath(filepath)}:")
+    for col in df.columns:
+        val = sample[col]
+        print(f"🔹 {col}: {val}")
+
+    return df
